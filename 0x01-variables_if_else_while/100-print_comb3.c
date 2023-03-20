fc1861a0356a5038ff735927f75effbbe068fe7d
Josephat Ngugi <josephatn10@gmail.com>
2023-03-20 13:12:58 -0700

    print all possible combinations of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/* more headers go here */
+
+/**
+ * main - Entry point
+ * print all possible combinations of two digits
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = i + 1; j < 59; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56 || j != 57)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
