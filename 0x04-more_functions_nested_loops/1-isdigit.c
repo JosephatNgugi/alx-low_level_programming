bf3d44ed73b02550232bc71adabd990b3774decb
JosephatNgugi <josephatn10@gmail.com>
2023-03-23 16:55:06 +0000

    checks if a character is a digit 0 through 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+/**
+ * _isdigit - checks if a character is a digit 0 through 9
+ * @c: The character to check
+ *
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (c == i)
+			return (1);
+	}
+
+	return (0);
+}
