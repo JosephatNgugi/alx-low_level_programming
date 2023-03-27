b1f47b360fc8c137d052e0f11fa62aefced60b32
Josephat Ngugi <josephatn10@gmail.com>
2023-03-27 13:30:14 -0700

    generate random passwords

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -11,18 +11,19 @@
 
 int main(void)
 {
-    char password[PASSWORD_LENGTH + 1];
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$?*#";
+	char password[PASSWORD_LENGTH + 1];
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!$?*#";
 
-    srand(time(0));
+	srand(time(0));
 
-    for (int i = 0; i < PASSWORD_LENGTH; i++) {
-        password[i] = charset[rand() % sizeof(charset)];
-    }
+	for (int i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		password[i] = charset[rand() % sizeof(charset)];
+	}
 
-    password[PASSWORD_LENGTH] = '\0';
+	password[PASSWORD_LENGTH] = '\0';
 
-    printf("%s\n", password);
+	printf("%s\n", password);
 
-    return 0;
+	return 0;
 }
