3e1a7cd79e028e13ffd161277f52589ff857f22b
JosephatNgugi <Josephatn10@gmail.com>
2023-03-28 17:42:31 +0300

    print every other character starting with the first

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of a string starting
+ * with the first character
+ * @str: String to be printed
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0';)
+	{
+		_putchar(str[i]);
+		i = i + 2;
+	}
+
+	_putchar('\n');
+}
