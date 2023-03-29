be1e600989d35ca6d65e8c3c37474267d87eb3fa
JosephatNgugi <josephatn10@gmail.com>
2023-03-29 15:08:02 +0000

    append a string to another string with a max number of bytes

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+/**
+ * _strcat - concatenates two strings by appending the src
+ * to dest string using specified number of bytes,
+ * @dest: Pointer to the dest character array
+ * @src: Pointer to the src character array
+ * @n: Number of bytes
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
