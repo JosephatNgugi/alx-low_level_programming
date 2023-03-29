063bccf6f248d492765be674a65c1004ba136953
JosephatNgugi <josephatn10@gmail.com>
2023-03-29 14:16:48 +0000

    appends one string to another

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+/**
+ * _strcat - concatenates two strings by appending the src
+ * to dest string,
+ * @dest: Pointer to the dest character array
+ * @src: Pointer to the src character array
+ *
+ * Return: A pointer to the resulting string dest.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+
+	dest[i] = '\0';
+
+	return dest;
+}
