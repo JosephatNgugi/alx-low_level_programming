6ecb3d9db69a62761b38bcd29ec223f912668f05
JosephatNgugi <Josephatn10@gmail.com>
2023-04-14 14:04:03 +0300

    Fix: concatenate both strings and checks if string is NULL

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
-#include <string.h>
 
+char *_memcpy(char *dest, char *src, unsigned int n);
+int _strlen(char *s);
 /**
  * string_nconcat - concatenates two strings
  * @s1: First string
@@ -14,10 +15,15 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *str;
-	unsigned int len_str, len_s1, len_s2, i;
+	unsigned int len_str, len_s1, len_s2;
 
-	len_s1 = strlen(s1);
-	len_s2 = strlen(s2);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	len_s1 = _strlen(s1);
+	len_s2 = _strlen(s2);
 
 	if (n < len_s2)
 		len_s2 = n;
@@ -28,13 +34,48 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; i < len_s1; i++)
-		str[i] = s1[i];
-
-	len_s1++;
-	for (i = 0; i < len_str; i++)
-		str[len_s1 + i] = s2[i];
-	str[len_s1 + i] = '\0';
+	_memcpy(str, s1, len_s1);
+	_memcpy(str + len_s1, s2, len_s2);
+	str[len_s1 + len_s2] = '\0';
 
 	return (str);
+}
+
+/**
+ * _strlen - returns the length of a string.
+ *@s: The string to check
+ *
+ * Return: The length of string s
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
+
+/**
+ * _memcpy - copies from memory area source to memory area destination
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
+ * @n: size of memory area to be copied
+ *
+ * Return: Pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
