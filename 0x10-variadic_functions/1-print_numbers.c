cac6b5712f97026fca130ee0212b082318c376d1
Josephat Ngugi <josephatn10@gmail.com>
2023-04-20 15:19:59 +0000

    print all numbers passed to a function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers followed by a new line
+ * @separator: Pointer to the string to be printed
+ * @n: Number of parameters passed to the function
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list params;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+
+	if (separator == NULL)
+		printf(" ");
+
+	va_start(params, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i - (n - 1) == 0)
+			printf("%d", va_arg(params, int));
+		else
+			printf("%d%c ", va_arg(params, int), *separator);
+	}
+	printf("\n");
+}
