9b21ac7b8b55f0d9fbbbf44571800e32e6b9222e
Josephat Ngugi <josephatn10@gmail.com>
2023-04-20 17:54:53 +0000

    boiler plate code

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints anything
+ * @format: List of arguments passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	va_list params;
+
+	va_start(params, n);
+	va_end(params)
+}
