59bd3a89659e000714afce65ce8d348fd544facd
JosephatNgugi <Josephatn10@gmail.com>
2023-04-21 14:23:48 +0300

    Fix: Check if one of the string is NULL

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -10,19 +10,22 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list params;
 	unsigned int i;
+	char *str_arr;
 
 	va_start(params, n);
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(params, char *));
-	
+		str_arr = va_arg(params, char *);
+		if (str_arr == NULL)
+			str_arr = "(nil)";
+		printf("%s", str_arr);
+
 		if (separator)
 		{
-			if (i - (n - 1) == 0)
-				printf("\n");
-			else
+			if (i - (n - 1) != 0)
 				printf("%s", separator);
 		}
 	}
 	va_end(params);
+	printf("\n");
 }
