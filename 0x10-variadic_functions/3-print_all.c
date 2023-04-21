f1c556be2658d840e22381fd2676510ca065dd74
JosephatNgugi <Josephatn10@gmail.com>
2023-04-21 14:40:13 +0300

    function to print any format

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -8,7 +8,37 @@
 void print_all(const char * const format, ...)
 {
 	va_list params;
+	char *str_arr;
+	int i = 0;
 
-	va_start(params, n);
-	va_end(params)
+	va_start(params, format);
+	while (format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(params, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(params, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(params, double));
+				break;
+			case 's':
+				str_arr = va_arg(params, char *);
+				if (str_arr == NULL)
+					str_arr = "(nil)";
+				printf("%s", str_arr);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		i++;
+		if (format[i] != '\0')
+			printf(", ");
+	}
+	va_end(params);
+	printf("\n");
 }
