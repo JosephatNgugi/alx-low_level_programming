c3fd254c906d6ef6de91c639ad9e613b1ce2dddd
JosephatNgugi <Josephatn10@gmail.com>
2023-04-28 11:41:14 +0300

    create a constructor function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void _constructor()__attribute__((constructor));
+/**
+ * _constructor - Prints a String of text before the main
+ * function is executed.
+ */
+void _constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
