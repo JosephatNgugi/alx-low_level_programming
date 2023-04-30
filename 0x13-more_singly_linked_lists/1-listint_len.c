18696bb1bcdcd358d25cf453ee35fe9ca81d3449
Josephat Ngugi <josephatn10@gmail.com>
2023-04-30 09:56:05 -0700

    compute the length of a linked list

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,21 @@
+
 #include "lists.h"
 
 /**
- * print_listint - Prints all the elements of a listint_t list.
+ * listint_len - Computes the number of elements of a listint_t list.
  * @h: Pointer to the first node element of the list
  *
  * Return: The number of nodes
  */
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t count = 0;
 	const listint_t *node;
 
 	node = h;
+
 	while (node != NULL)
 	{
-		printf("%d\n", node->n);
 		count++;
 		node = node->next;
 	}
