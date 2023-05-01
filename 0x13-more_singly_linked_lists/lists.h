b24d03ce266e7c361f7fdefeee8ffaac6e20cde6
Josephat Ngugi <josephatn10@gmail.com>
2023-05-01 15:54:16 +0000

    Fix: Betty compliance to lists header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -8,14 +8,15 @@
 /******** Structs and Typedefs ********/
 
 /**
- * struct listint_s - singly linked list 
+ * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
  *
  */
-typedef struct listint_s{
+typedef struct listint_s
+{
 	int n;
 	struct listint_s *next;
 } listint_t;
